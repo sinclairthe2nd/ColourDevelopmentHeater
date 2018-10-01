ef007f1bef31ba5e603a13c702b0d4effabf8b92
MARC <accx77@googlemail.com>
2018-10-01 16:17:56 +0200

    This adds more efficient mapping code and rearanges the code to please the mighty compiler.

--- AVR-Straight-C/main.c
+++ AVR-Straight-C/main.c
@@ -1,6 +1,5 @@
 #include <avr/io.h>
 #include <util/delay.h>
-
 //Configutations
 
 #define PORT_7_SEGMENT PORTD
@@ -8,10 +7,19 @@
 #define DDR_MULTI DDRB
 #define PORTMULTI PORTB
 
+// Defines for the SegmentCases that map the Number to the Output Pins
+#define CASE0 0b00000010
+#define CASE1 0b11001111
+#define CASE2 0b00101001
+#define CASE3 0b01001001
+#define CASE4 0b11000101
+#define CASE5 0b01010001
+#define CASE6 0b00010001
+#define CASE7 0b11001011
+#define CASE8 0b00000000
+#define CASE9 0b01000001
 
 int NUMBER = 0;
-int INPUTNUMBER =0;
-
 
 void initADC()
 {
@@ -33,12 +41,12 @@ void initADC()
             16 MHz   128 (125kHz)
            Below example set prescaler to 128 for mcu running at 8MHz
            (check the datasheet for the proper bit values to set the prescaler)
-  */
-  //Guuddygit
-  // 8-bit resolution
-  // set ADLAR to 1 to enable the Left-shift result (only bits ADC9..ADC2 are available)
-  // then, only reading ADCH is sufficient for 8-bit results (256 values)
+  
 
+            8-bit resolution
+            set ADLAR to 1 to enable the Left-shift result (only bits ADC9..ADC2 are available)
+            then, only reading ADCH is sufficient for 8-bit results (256 values)
+*/
   ADMUX =
             (1 << ADLAR) |     // left shift result
             (1 << REFS1) |     // Sets ref. voltage to 1.1V, bit 1
@@ -54,6 +62,60 @@ void initADC()
             (1 << ADPS1) |     // set prescaler to 64, bit 1
             (0 << ADPS0);      // set prescaler to 64, bit 0
 }
+
+void Display(int NUMBER)
+{
+
+//This will map the number in number to PORT_7_Segment very elegantly.
+int Segmapper[10] = {CASE0,CASE1,CASE2,CASE3,CASE4,CASE5,CASE6,CASE7,CASE8,CASE9};
+PORT_7_SEGMENT = Segmapper[NUMBER];
+
+/*
+switch (NUMBER)//Maps the number in number to the display rather crudely (its very flexible and readable though)
+      {
+         case 0:
+         PORT_7_SEGMENT=0b00000010; //0b00000010
+         break;
+
+         case 1:
+         PORT_7_SEGMENT=0b11001111; //0b11001111
+         break;
+
+         case 2:
+         PORT_7_SEGMENT=0b00101001; //0b00101001
+         break;
+
+         case 3:
+         PORT_7_SEGMENT=0b01001001; //0b01001001
+         break;
+
+         case 4:
+         PORT_7_SEGMENT=0b11000101; //0b11000101
+         break;
+
+         case 5:
+         PORT_7_SEGMENT=0b01010001; //0b01010001
+         break;
+
+         case 6:
+         PORT_7_SEGMENT=0b00010001; //0b00010001
+         break;
+
+         case 7:
+         PORT_7_SEGMENT=0b11001011; //0b11001011
+         break;
+
+         case 8:
+         PORT_7_SEGMENT=0b00000000; //0b00000000
+         break;
+
+         case 9:
+         PORT_7_SEGMENT=0b01000001; //0b01000001
+         break;
+      }
+   */   
+}
+
 
 void SevenSegment(int INPUTNUMBER)
 {
@@ -78,52 +140,7 @@ if(INPUTNUMBER > 9) //Conditioning of the Inputnumber
         Display(NUMBER);
       }
 }
-void Display(int NUMBER)
-{
 
-switch (NUMBER)//Maps the number in number to the display rather crudely (its very flexible and readable though)
-      {
-         case 0:
-         PORT_7_SEGMENT=0b00000010;
-         break;
-
-         case 1:
-         PORT_7_SEGMENT=0b11001111;
-         break;
-
-         case 2:
-         PORT_7_SEGMENT=0b00101001;
-         break;
-
-         case 3:
-         PORT_7_SEGMENT=0b01001001;
-         break;
-
-         case 4:
-         PORT_7_SEGMENT=0b11000101;
-         break;
-
-         case 5:
-         PORT_7_SEGMENT=0b01010001;
-         break;
-
-         case 6:
-         PORT_7_SEGMENT=0b00010001;
-         break;
-
-         case 7:
-         PORT_7_SEGMENT=0b11001011;
-         break;
-
-         case 8:
-         PORT_7_SEGMENT=0b00000000;
-         break;
-
-         case 9:
-         PORT_7_SEGMENT=0b01000001;
-         break;
-      }
-}
 
 int main(void)
 {
@@ -131,18 +148,20 @@ int main(void)
    DDR_7_SEGMENT=0b11111111;    //All output
    PORT_7_SEGMENT=0xFF;   //All segments off
    DDR_MULTI=0b11111111;  //All Multiplexing  pins are outputs
-   DDRC = 0b00000001;   
+   DDRC = 0b00000001;
+   PORTC = 0;
    initADC();
    while(1)               //loop forever
    {
 
  
- // take 100x 8-bit samples and calculate a rolling average of the last 15 samples
+
   
  int temp = 0;        // Temperature
  float linearm = 0.68;    // m for the Calibration equation
  float linearb = 6.25;    // b for the Calibration equation
  int setpoint = 0;
+
  ADCSRA |= (1 << ADSC);         // start ADC measurement
  while (ADCSRA & (1 << ADSC) ); // wait till conversion complete 
  
@@ -158,10 +177,10 @@ int main(void)
     {
     setpoint = 38;
     }
-
  if(temp<setpoint) 
     {
     PORTC = (1<< PORTC0);  // Turn Output on  
+
     }
     else
     {
